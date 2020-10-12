490b2f44efd07a4b8fe18c51b3499a8397d4300d
NoRoboto <daniel@sirius.utp.edu.co>
2020-10-12 15:33:06 -0500

    Add image notifications data type

--- src/data/notifications.js
+++ src/data/notifications.js
@@ -6,9 +6,7 @@ export const notificationsData = [
     cardTitle: 'File Updated',
     cardDescription: 'Your file has been successfully uploaded.',
     cardTime: '1 day',
-    iconSize: 12,
-    icon: faFileUpload,
-    iconProps: {color: 'white'},
+    iconProps: {icon: faFileUpload, color: 'white', iconSize: 12},
     textVariants: {title: 'browseCardLabelTitle', body: 'browseCardLabel'},
     circleProps: {circleSize: 30, variant: 'info'},
     onPress: () => console.warn('1'),
@@ -18,9 +16,7 @@ export const notificationsData = [
     cardTitle: 'File Deleted',
     cardDescription: 'Your file has been successfuly deleted.',
     cardTime: '1 day',
-    iconSize: 12,
-    icon: faTrash,
-    iconProps: {color: 'white'},
+    iconProps: {icon: faTrash, color: 'white', iconSize: 12},
     textVariants: {title: 'browseCardLabelTitle', body: 'browseCardLabel'},
     circleProps: {circleSize: 30, variant: 'error'},
     onPress: () => console.warn('2'),
@@ -30,9 +26,7 @@ export const notificationsData = [
     cardTitle: 'Papssword Reset',
     cardDescription: 'Your password has been restored successfuly.',
     cardTime: '1 day',
-    iconSize: 12,
-    icon: faSync,
-    iconProps: {color: 'white'},
+    iconProps: {icon: faSync, color: 'white', iconSize: 12},
     textVariants: {title: 'browseCardLabelTitle', body: 'browseCardLabel'},
     circleProps: {circleSize: 30, variant: 'alert'},
     onPress: () => console.warn('3'),
@@ -45,9 +39,11 @@ export const simpleNotificationsData = [
     cardTitle: 'File Updated',
     cardDescription: 'Your file has been successfully uploaded.',
     cardTime: '1 day',
-    iconSize: 13,
-    icon: faFileUpload,
-    iconProps: {color: 'notificationInfoWhiteFade'}, //restyle theme object map
+    iconProps: {
+      icon: faFileUpload,
+      color: 'notificationInfoWhiteFade',
+      iconSize: 13,
+    }, //restyle theme object map
     textVariants: {title: 'simpleCardLabelTitle', body: 'simpleCardLabel'},
     circleProps: {circleSize: 30, variant: 'simple'},
     onPress: () => console.warn('1'),
@@ -57,9 +53,11 @@ export const simpleNotificationsData = [
     cardTitle: 'File Deleted',
     cardDescription: 'Your file has been successfuly deleted.',
     cardTime: '1 day',
-    iconSize: 12,
-    icon: faTrash,
-    iconProps: {color: 'notificationAlertBackground'},
+    iconProps: {
+      icon: faTrash,
+      color: 'notificationAlertBackground',
+      iconSize: 12,
+    },
     textVariants: {title: 'simpleCardLabelTitle', body: 'simpleCardLabel'},
     circleProps: {circleSize: 30, variant: 'simpleAlert'},
     onPress: () => console.warn('2'),
@@ -69,11 +67,48 @@ export const simpleNotificationsData = [
     cardTitle: 'Password Reset',
     cardDescription: 'Your password has been restored successfuly.',
     cardTime: '1 day',
-    iconSize: 12,
-    icon: faSync,
-    iconProps: {color: 'notificationErrorBackground'},
+    iconProps: {
+      icon: faSync,
+      color: 'notificationErrorBackground',
+      iconSize: 12,
+    },
     textVariants: {title: 'simpleCardLabelTitle', body: 'simpleCardLabel'},
     circleProps: {circleSize: 30, variant: 'simpleError'},
     onPress: () => console.warn('3'),
+  },
+];
+
+export const simpleNotificationsImageData = [
+  {
+    variant: 'simple',
+    cardTitle: 'Jenny Osxal',
+    cardDescription:
+      'Please find attached .XD file with the document containing the ...',
+    cardTime: '1 hour',
+    iconProps: {
+      color: 'notificationInfoWhiteFade',
+      iconSize: 13,
+      image:
+        'https://cdn.pixabay.com/photo/2014/05/03/00/50/flower-child-336658_960_720.jpg',
+    }, //restyle theme object map
+    textVariants: {title: 'simpleCardLabelTitle', body: 'simpleCardLabel'},
+    circleProps: {circleSize: 30, variant: 'simple'},
+    onPress: () => console.warn('1'),
+  },
+  {
+    variant: 'simple',
+    cardTitle: 'Camila something',
+    cardDescription:
+      'Hey can you check the latest documents regarding notifications?',
+    cardTime: '30 mins',
+    iconProps: {
+      color: 'notificationAlertBackground',
+      iconSize: 12,
+      image:
+        'https://cdn.pixabay.com/photo/2017/09/01/21/53/blue-2705642_960_720.jpg',
+    },
+    textVariants: {title: 'simpleCardLabelTitle', body: 'simpleCardLabel'},
+    circleProps: {circleSize: 30, variant: 'simpleAlert'},
+    onPress: () => console.warn('2'),
   },
 ];
